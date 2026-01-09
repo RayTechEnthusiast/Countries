0798bee24193938d0d1852f1028181837b0ff2ea
RayTechEnthusiast <tru3parzival@gmail.com>
2026-01-09 19:28:49 +0000

    Did smth

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,6 +1,27 @@
 public class Country
 {
+  private String name;
+  private String capital;
+  private String language;
+  private String imageFile;
+
   // add private instance variables for the name, capital, language, and image file.
+
+  public Country(String name, String capital, String language, String imageFile){
+    this.name = name;
+    this.capital = capital;
+    this.language = language;
+    this.imageFile = imageFile;
+  }
+
+  public Country(){
+
+    this.name = "null";
+    this.capital = "null";
+    this.language = "null";
+    this.imageFile = "null";
+
+  }
 
   // add constructors
 
